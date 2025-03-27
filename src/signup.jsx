e957286b91056b9e2ea1cf7e5a4997de6b9049c4
robinjeffry <robinjeffry655@gmail.com>
2025-03-27 10:47:29 +0530

    updated2

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Modal.css";
+import "./modal.css";
 
 const Signup = ({ closeModal }) => {
   const [formData, setFormData] = useState({
